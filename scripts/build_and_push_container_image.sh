f4370453d8ef43c810dfdd4a31f64254a838658e
Jernej Kos <jernej@kos.mx>
2025-08-14 10:31:03 +0200

    ROFLize the Talos agent

---
+++ scripts/build_and_push_container_image.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [[ -z "${TARGET_IMAGE}" ]]; then
+    echo "ERROR: Required environment variable TARGET_IMAGE not set."
+    exit 1
+fi
+
+BUILDER_NAME=buildkit_23
+BUILDER_IMAGE=moby/buildkit:v0.23.2
+SOURCE_DATE_EPOCH=1755248916
+
+# Ensure we use the right buildkit for reproducibility.
+if ! docker buildx inspect ${BUILDER_NAME} &>/dev/null; then
+    docker buildx create --use --driver-opt image=${BUILDER_IMAGE} --name ${BUILDER_NAME}
+fi
+
+# Build the container in a reproducible way.
+METADATA_FILE=$(mktemp)
+
+docker buildx build \
+	--builder ${BUILDER_NAME} \
+	--no-cache \
+	--provenance false \
+	--build-arg SOURCE_DATE_EPOCH="${SOURCE_DATE_EPOCH}" \
+	--output type=registry,name=${TARGET_IMAGE},rewrite-timestamp=true \
+	--metadata-file "${METADATA_FILE}" \
+	.
+
+# Output the image digest.
+IMAGE_NAME=$(jq -r '."image.name" + "@" + ."containerimage.digest"' "${METADATA_FILE}")
+if [[ -n "${OUTPUT_IMAGE_NAME_PATH}" ]]; then
+    echo "${IMAGE_NAME}" > ${OUTPUT_IMAGE_NAME_PATH}
+fi
+echo "${IMAGE_NAME}"
---
+++ scripts/verify_container_image.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+COMPOSE_FILE=$(yq -r '.artifacts.container.compose' rofl.yaml)
+TALOS_AGENT_IMAGE=$(yq -r '.services."talos-agent".image' ${COMPOSE_FILE})
+
+if [[ "${TALOS_AGENT_IMAGE}" != "${EXPECTED_TALOS_AGENT_IMAGE}" ]]; then
+  echo "Talos agent image mismatch:"
+  echo ""
+  echo "  Configured in ${COMPOSE_FILE}:"
+  echo "    ${TALOS_AGENT_IMAGE}"
+  echo ""
+  echo "  Built locally:"
+  echo "    ${EXPECTED_TALOS_AGENT_IMAGE}"
+  echo ""
+  exit 1
+fi
